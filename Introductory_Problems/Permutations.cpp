a45e5873ea639afbb934669b4e6499e7b6dc0df3
anassodor1@gmail.com <asodor@c4r7p10.1337.ma>
2024-01-22 09:08:04 +0100

    Done

--- Introductory_Problems/Permutations.cpp
+++ Introductory_Problems/Permutations.cpp
@@ -1,23 +1,23 @@
 #include <iostream>
-
+ 
 int main()
 {
     int n;
     std::cin >> n;
-
+ 
     if (n == 2 || n == 3)
     {
         std::cout << "NO SOLUTION\n";
         return 0;
     }
-
+ 
     int i = 0;
     int print = n - 1;
     while(i < n / 2)
     {
         std::cout << print << " ";
         print -= 2;
-
+ 
         i++;
     }
     print = n;
@@ -26,7 +26,7 @@ int main()
     {
         std::cout << print << " ";
         print -= 2;
-
+ 
         i++;
     }
     std::cout << std::endl;
